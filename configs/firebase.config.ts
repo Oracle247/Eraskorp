529a47a2057afd2582a319003da75788b5ef4d3a
Codes Oracle <94334497+Oracle247@users.noreply.github.com>
2023-07-30 21:37:27 +0100

    update firebase config

--- configs/firebase.config.ts
+++ configs/firebase.config.ts
@@ -10,12 +10,12 @@ import { getStorage } from "firebase/storage";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_RWmWVRuQC-785kQ40sEdwNnz6Yb6sBM",
-  authDomain: "brilliant-brains-7e04f.firebaseapp.com",
-  projectId: "brilliant-brains-7e04f",
-  storageBucket: "brilliant-brains-7e04f.appspot.com",
-  messagingSenderId: "838066145252",
-  appId: "1:838066145252:web:503b26af450ef5b3dd69f1",
+  apiKey: "AIzaSyAs4asX5YyG0Lgmmv_QP2_iOvx0rcDvt6k",
+  authDomain: "eraskon-5fdff.firebaseapp.com",
+  projectId: "eraskon-5fdff",
+  storageBucket: "eraskon-5fdff.appspot.com",
+  messagingSenderId: "209976174946",
+  appId: "1:209976174946:web:61fbe92a87143b3858a43b"
 };
 
 // Initialize Firebase
